1580b9e2bab25628dd8ed421c11ebf8480cee2d2
Robert Martinu <robert.s.martinu@gmail.com>
2018-03-12 02:09:52 +0100

    Start Somewhere

---
+++ ADS1Hash/src/ads1hash/ADS1Hash.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ads1hash;
+
+/**
+ *
+ * @author Robert Martinu
+ */
+public class ADS1Hash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
